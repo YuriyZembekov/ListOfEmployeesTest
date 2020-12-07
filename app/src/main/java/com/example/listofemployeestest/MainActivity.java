448d36dfda5ca50f572d62d176f2f5c5a835e2e7
Yuriy Zembekov <l2sws@yandex.ru>
2020-12-07 13:10:37 +0400

    version 1.00 Add TestConnection.java

--- app/src/main/java/com/example/listofemployeestest/MainActivity.java
+++ app/src/main/java/com/example/listofemployeestest/MainActivity.java
@@ -8,6 +8,7 @@ import com.example.listofemployeestest.adapter.EmployeesAdapter;
 import com.example.listofemployeestest.javaclasses.entity.Employee;
 import com.example.listofemployeestest.javaclasses.logic.BaseLogic;
 import com.example.listofemployeestest.javaclasses.logic.NameComparatorForEmployee;
+import com.example.listofemployeestest.javaclasses.logic.TestConnection;
 
 import java.util.Collections;
 import java.util.List;
@@ -16,30 +17,37 @@ import java.util.concurrent.ExecutionException;
 // Основной класс-активность, отображает информацию на экране
 public class MainActivity extends Activity {
 
-    private List<Employee> emloyees = null;
+    private List<Employee> employees = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         initListOfEmployees();
-        RecyclerView employeesRecyclerView =
-                (RecyclerView) findViewById(R.id.recycler_view_for_employees);
-        EmployeesAdapter employeesAdapter = new EmployeesAdapter(this, emloyees);
-        employeesRecyclerView.setAdapter(employeesAdapter);
-
+        if (employees != null) {
+            setContentView(R.layout.activity_main);
+            initListOfEmployees();
+            RecyclerView employeesRecyclerView =
+                    (RecyclerView) findViewById(R.id.recycler_view_for_employees);
+            EmployeesAdapter employeesAdapter = new EmployeesAdapter(this, employees);
+            employeesRecyclerView.setAdapter(employeesAdapter);
+        } else {
+            setContentView(R.layout.no_connection);
+        }
     }
 
     // метод инициализирует и сортирует список сотрудников
-    private void initListOfEmployees(){
-        AsyncTaskForNetworkConnection asyncTaskGetListOfEmployees =
-                new AsyncTaskForNetworkConnection();
-        asyncTaskGetListOfEmployees.execute();
-        try {
-            emloyees = asyncTaskGetListOfEmployees.get();
-            Collections.sort(emloyees, new NameComparatorForEmployee());
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
+    private void initListOfEmployees() {
+        if (new TestConnection(getApplicationContext()).isConnectingToInternet()) {
+            AsyncTaskForNetworkConnection asyncTaskGetListOfEmployees =
+                    new AsyncTaskForNetworkConnection();
+            asyncTaskGetListOfEmployees.execute();
+
+            try {
+                employees = asyncTaskGetListOfEmployees.get();
+                Collections.sort(employees, new NameComparatorForEmployee());
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
